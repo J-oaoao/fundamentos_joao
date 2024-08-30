84a5ccc0aff74772ed5b043b3cd7ed5232200ced
J-oaoao <jfsg0800@gmail.com>
2024-08-30 10:39:19 -0300

    primeiros exercicios

---
+++ python_atividades/boas_vinda.py
@@ -0,0 +1,2 @@
+nome = input ("bom dia, como voce se chama?")
+print("Olá,{}!".format(nome),"agradeço as infos")
---
+++ python_atividades/input.py
@@ -0,0 +1,5 @@
+nome = input('como voce se chama?')
+idade = input('qual é a sua idade?')
+peso = input('Qual é o seu peso?')
+print (nome, idade, peso)
+print ('Legal! Seu nome é',nome,',voce tem',idade,'anos e pesa ',peso,' quilos!')
