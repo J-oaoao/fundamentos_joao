a76827b8979389a12edd915b53148906ff6d7c6d
J-oaoao <jfsg0800@gmail.com>
2024-09-11 09:07:24 -0300

    testes e treinamentos

---
+++ python_atividades/if_elif.py
@@ -0,0 +1,11 @@
+valor = input("digite um valor de 0 a 3:")
+if (valor == 0):
+    print("valor é 0")
+elif (valor == 1):
+    print ("valor é 1")
+elif (valor == 2):
+    print ("valor é 2")
+elif (valor == 3):
+    print ("valor é 3")
+else:
+    print("O valor não está entre 0 e 3")
--- python_atividades/ws.py
+++ python_atividades/ws.py
@@ -1,14 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-link = "https://www.google.com/search?q=Jo%C3%A3o+Felipe&"
+link = "https://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
+#headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36"}
+
 requisicao = requests.get(link)
 print(requisicao)
-# print(requisicao.text)
+
 site = BeautifulSoup (requisicao.text, "html.parser")
 # print(site.prettify())
 
-# mais simples print(site.title)
-# mais preciso titulo = site.find("title") ^ print(titulo)
-input = site.find_all("input")
-print(input)
+titulo = site.find_all("a")
+print(titulo)
+
+pesquisa2 = site.find("a", class_="row")
+print(pesquisa2)
